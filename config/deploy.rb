3c7a13792bdb73f82a5d0b0f09a8f6c1605c3e6d
HeeL <parizhskiy@gmail.com>
2014-02-13 22:28:07 +0100

    bring the old repo github

--- config/deploy.rb
+++ config/deploy.rb
@@ -77,7 +77,7 @@ set :scm,             :git
 # set :repository,      "ssh://#{user}@#{deploy_server}/home/#{user}/git/#{application}.git"
 
 ## Если ваш репозиторий в GitHub, используйте такую конфигурацию
-set :repository,    "git@bitbucket.org:heel/expert.git"
+set :repository,    "git@github.com:HeeL/expert.git"
 
 ## --- Ниже этого места ничего менять скорее всего не нужно ---
 
